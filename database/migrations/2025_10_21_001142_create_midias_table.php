5934d6128fa96a13d7b1843c4bcaf6edd7bf3872
Gustavo <gwgustavowagner@hotmail.com>
2025-10-20 21:45:22 -0300

    adiciona tabelas Midias e Categorias com relações de chave estrangeira

---
+++ database/migrations/2025_10_21_001142_create_midias_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('midias', function (Blueprint $table){
+            $table->id();
+            $table->year('ano')->default(Carbon::now()->year);
+            $table->string('titulo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('midias');
+    }
+};
--- database/migrations/2025_10_21_002411_foreign_keys.php
+++ database/migrations/2025_10_21_002411_foreign_keys.php
@@ -11,7 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('midias', function (Blueprint $table) {
+            $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade');
+        });
     }
 
     /**
